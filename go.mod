f13d06ec8b57805323e9d112a7ba241b6f1237ef
lizw220 <lizw220@chinaunicom.cn>
2023-08-28 17:19:44 +0800

    add leader election

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module crd-controller
 go 1.19
 
 require (
+	github.com/google/uuid v1.3.0
 	k8s.io/api v0.28.0
 	k8s.io/apimachinery v0.28.0
 	k8s.io/client-go v0.28.0
@@ -24,7 +25,6 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
--- main.go
+++ main.go
@@ -17,12 +17,21 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
 	"time"
 
+	"github.com/google/uuid"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog/v2"
 
 	"crd-controller/pkg/controller"
@@ -32,13 +41,24 @@ import (
 )
 
 var (
-	masterURL  string
 	kubeconfig string
+
+	pprofAddr   string
+	enablePprof bool
+
+	leaseLockName      string
+	leaseLockNamespace string
+	leaseLockId        string
 )
 
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
-	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+	flag.StringVar(&pprofAddr, "pprof-addr", ":8090", "The address the pprof binds to.")
+	flag.BoolVar(&enablePprof, "enable-pprof", false, "Enable pprof for crd-controller.")
+	flag.StringVar(&leaseLockId, "lease-lock-id", fmt.Sprintf("crd-controller-%v", uuid.New().String()),
+		"lease-lock-id determines the name of the resource that leader election will use for holding the leader lock, Default is crd-controller-uuid.")
+	flag.StringVar(&leaseLockName, "lease-lock-name", "", "the lease lock resource name")
+	flag.StringVar(&leaseLockNamespace, "lease-lock-namespace", "", "the lease lock resource namespace")
 }
 
 func main() {
@@ -48,12 +68,22 @@ func main() {
 	ctx := signals.SetupSignalHandler()
 	logger := klog.FromContext(ctx)
 
-	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	// 1、若需要，开启 pprof
+	if enablePprof {
+		go func() {
+			if err := http.ListenAndServe(pprofAddr, nil); err != nil {
+				klog.Error(err, "unable to start pprof")
+			}
+		}()
+	}
+
+	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		logger.Error(err, "Error building kubeconfig")
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
 
+	// 2、创建kubeClient、crdClient客户端
 	kubeClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
 		logger.Error(err, "Error building kubernetes clientset")
@@ -65,18 +95,66 @@ func main() {
 		logger.Error(err, "Error building kubernetes clientset")
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
+
+	// 3、创建 kubeInformerFactory、crdInformerFactory
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
 	crdInformerFactory := informers.NewSharedInformerFactory(crdClient, time.Second*30)
 
+	// 4、创建 controller 实例
 	controller := controller.NewController(ctx, kubeClient, crdClient,
 		kubeInformerFactory.Apps().V1().Deployments(),
 		crdInformerFactory.Extension().V1().UnitedDeployments())
 
+	// 5、运行 kubeInformerFactory、crdInformerFactory
 	kubeInformerFactory.Start(ctx.Done())
 	crdInformerFactory.Start(ctx.Done())
 
-	if err = controller.Run(ctx, 2); err != nil {
-		logger.Error(err, "Error running controller")
-		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      leaseLockName,
+			Namespace: leaseLockNamespace,
+		},
+		Client: kubeClient.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: leaseLockId,
+		},
 	}
+
+	// 6、进行选主逻辑
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock: lock,
+		// IMPORTANT: you MUST ensure that any code you have that
+		// is protected by the lease must terminate **before**
+		// you call cancel. Otherwise, you could have a background
+		// loop still running and another process could
+		// get elected before your background loop finished, violating
+		// the stated goal of the lease.
+		ReleaseOnCancel: true,
+		LeaseDuration:   60 * time.Second,
+		RenewDeadline:   15 * time.Second,
+		RetryPeriod:     5 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+
+				// 7、选主成功，运行 controller 逻辑
+				if err = controller.Run(ctx, 2); err != nil {
+					logger.Error(err, "Error running controller")
+					klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+				}
+			},
+			OnStoppedLeading: func() {
+				// we can do cleanup here
+				klog.Infof("leader lost: %s", leaseLockId)
+				os.Exit(0)
+			},
+			OnNewLeader: func(identity string) {
+				// we're notified when new leader elected
+				if identity == leaseLockId {
+					// I just got the lock
+					return
+				}
+				klog.Infof("new leader elected: %s", identity)
+			},
+		},
+	})
 }
